223b09c6e7411f0dcffc9770cbb53988938a5c4b
sbenyoussef <soufiane.benyoussef@gmail.com>
2019-09-18 22:35:48 +0200

    Create jslinkForm.js

---
+++ jslinkForm.js
@@ -0,0 +1,29 @@
+var jslinkForm = function(){
+
+_onPreRender=function(ctx){
+};
+
+_onPostRender=function(ctx){
+};
+
+_run = function(){
+  // Create object that have the context information about the field that we want to change it's output render  
+    var ctx = {
+    Templates:{
+      },
+      OnPreRender:_onPreRender
+      OnPostRender:_onPostRender
+      
+    }; 
+ 
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides(ctx); 
+}
+
+return {
+run:_run
+}
+};
+
+//Run the jslink
+var myForm = new jslinkForm();
+myForm.run();
